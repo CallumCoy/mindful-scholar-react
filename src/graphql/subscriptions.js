489bb039b3109527c91d429246721cc464a7ab1a
CallumCoy <callumcoy@knights.ucf.edu>
2023-03-01 22:05:25 -0500

    Added database

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,72 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateNote = /* GraphQL */ `
+  subscription OnCreateNote($filter: ModelSubscriptionNoteFilterInput) {
+    onCreateNote(filter: $filter) {
+      ScholarshipName
+      ExpirationDate
+      Amount
+      ApplicationLink
+      CitizenshipStatus
+      CollegeLevel
+      Description
+      Ethnicity
+      Interests
+      maxGPA
+      minGPA
+      Provider
+      StateOfResidency
+      TypeOfProgram
+      id
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateNote = /* GraphQL */ `
+  subscription OnUpdateNote($filter: ModelSubscriptionNoteFilterInput) {
+    onUpdateNote(filter: $filter) {
+      ScholarshipName
+      ExpirationDate
+      Amount
+      ApplicationLink
+      CitizenshipStatus
+      CollegeLevel
+      Description
+      Ethnicity
+      Interests
+      maxGPA
+      minGPA
+      Provider
+      StateOfResidency
+      TypeOfProgram
+      id
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteNote = /* GraphQL */ `
+  subscription OnDeleteNote($filter: ModelSubscriptionNoteFilterInput) {
+    onDeleteNote(filter: $filter) {
+      ScholarshipName
+      ExpirationDate
+      Amount
+      ApplicationLink
+      CitizenshipStatus
+      CollegeLevel
+      Description
+      Ethnicity
+      Interests
+      maxGPA
+      minGPA
+      Provider
+      StateOfResidency
+      TypeOfProgram
+      id
+      createdAt
+      updatedAt
+    }
+  }
+`;
