7655fe1f8b171cd9aad10485fbf94354d847a6b7
CallumCoy <callumcoy@knights.ucf.edu>
2023-03-05 12:45:29 -0500

    fixed sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { elastic as Menu } from 'react-burger-menu'
+import { scaleRotate as Menu } from 'react-burger-menu'
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 
@@ -9,99 +9,101 @@ export class SideBar extends React.Component {
   render () {
 
     return (
-      <Menu width={'400px'}>
-        <div>
-            <Form.Label className="light-text">Scholarship</Form.Label>
-            <InputGroup className="mb-3">
-                <Form.Control
-                placeholder="Scholarship"
-                aria-label="Scholarship"
-                aria-describedby="basic-addon1"
-                />
-            </InputGroup>
+      <Menu width={'400px'} pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" } >
+        <div class='scrolling'>
+            <Form.Label class="scrolling" className="light-text scrolling">Scholarship</Form.Label>
+                <div>
+                <InputGroup className="mb-3">
+                    <Form.Control
+                    placeholder="Scholarship"
+                    aria-label="Scholarship"
+                    aria-describedby="basic-addon1"
+                    />
+                </InputGroup>
 
-            <br/>
+                <br/>
 
-            <Form.Label className="light-text">Application Date</Form.Label>
-            <input type="date" class="form-control"/>
+                <Form.Label className="light-text">Application Date</Form.Label>
+                <input type="date" class="form-control"/>
 
-            <br/>
+                <br/>
 
-            <Form.Label className="light-text">GPA</Form.Label>
-            <InputGroup className="mb-3">
-                <Form.Control
-                placeholder="4.0"
-                aria-label="GPA"
-                aria-describedby="basic-addon1"
-                />
-            </InputGroup>
+                <Form.Label className="light-text">GPA</Form.Label>
+                <InputGroup className="mb-3">
+                    <Form.Control
+                    placeholder="4.0"
+                    aria-label="GPA"
+                    aria-describedby="basic-addon1"
+                    />
+                </InputGroup>
 
-            <br/>
+                <br/>
 
-            <Form.Label className="light-text">Backer</Form.Label>
-            <InputGroup className="mb-3">
-                <Form.Control
-                placeholder="UCF"
-                aria-label="Backer"
-                aria-describedby="basic-addon1"
-                />
-            </InputGroup>
-            
-            <br/>
+                <Form.Label className="light-text">Backer</Form.Label>
+                <InputGroup className="mb-3">
+                    <Form.Control
+                    placeholder="UCF"
+                    aria-label="Backer"
+                    aria-describedby="basic-addon1"
+                    />
+                </InputGroup>
+                
+                <br/>
 
-            <Form.Label className="light-text">Amount</Form.Label>
-            <InputGroup className="mb-3">
-                <InputGroup.Text>$</InputGroup.Text>
-                <Form.Control
-                placeholder="1000"
-                aria-label="Amount"
-                aria-describedby="basic-addon1"
-                />
-            </InputGroup>
+                <Form.Label className="light-text">Amount</Form.Label>
+                <InputGroup className="mb-3">
+                    <InputGroup.Text>$</InputGroup.Text>
+                    <Form.Control
+                    placeholder="1000"
+                    aria-label="Amount"
+                    aria-describedby="basic-addon1"
+                    />
+                </InputGroup>
 
-            <br/>
+                <br/>
 
-            <Form.Label className="light-text">Interests</Form.Label>
-            <InputGroup className="mb-3">
-                <Form.Control
-                placeholder="Interests"
-                aria-label="Interests"
-                aria-describedby="basic-addon1"
-                />
-            </InputGroup>
+                <Form.Label className="light-text">Interests</Form.Label>
+                <InputGroup className="mb-3">
+                    <Form.Control
+                    placeholder="Interests"
+                    aria-label="Interests"
+                    aria-describedby="basic-addon1"
+                    />
+                </InputGroup>
 
-            <br/>
+                <br/>
 
-            <Form.Label className="light-text">Ethnicity</Form.Label>
-            <InputGroup className="mb-3">
-                <Form.Control
-                placeholder="Ethnicity"
-                aria-label="Ethnicity"
-                aria-describedby="basic-addon1"
-                />
-            </InputGroup>
+                <Form.Label className="light-text">Ethnicity</Form.Label>
+                <InputGroup className="mb-3">
+                    <Form.Control
+                    placeholder="Ethnicity"
+                    aria-label="Ethnicity"
+                    aria-describedby="basic-addon1"
+                    />
+                </InputGroup>
 
-            <br/>
+                <br/>
 
-            <Form.Label className="light-text">Citizenship Status</Form.Label>
-            <InputGroup className="mb-3">
-                <Form.Control
-                placeholder="Citizenship Status"
-                aria-label="Citizenship Status"
-                aria-describedby="basic-addon1"
-                />
-            </InputGroup>
+                <Form.Label className="light-text">Citizenship Status</Form.Label>
+                <InputGroup className="mb-3">
+                    <Form.Control
+                    placeholder="Citizenship Status"
+                    aria-label="Citizenship Status"
+                    aria-describedby="basic-addon1"
+                    />
+                </InputGroup>
 
-            <br/>
+                <br/>
 
-            <Form.Label className="light-text">State of Residency</Form.Label>
-            <InputGroup className="mb-3">
-                <Form.Control
-                placeholder="State of Residency"
-                aria-label="State of Residency"
-                aria-describedby="basic-addon1"
-                />
-            </InputGroup>
+                <Form.Label className="light-text">State of Residency</Form.Label>
+                <InputGroup className="mb-3">
+                    <Form.Control
+                    placeholder="State of Residency"
+                    aria-label="State of Residency"
+                    aria-describedby="basic-addon1"
+                    />
+                </InputGroup>
+            </div>
         </div>
       </Menu>
     );
