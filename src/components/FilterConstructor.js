654e8ef0f0459207d8d09b4ec2558534b81cf566
CallumCoy <callumcoy@knights.ucf.edu>
2023-03-25 13:45:55 -0400

    MIssed a filter, and cleaned up typos

--- src/components/FilterConstructor.js
+++ src/components/FilterConstructor.js
@@ -5,85 +5,99 @@ export function filterSet(scholarships, filters) {
 
   scholarships.forEach((scholarship) => {
     try {
-      scholarship.show = name(scholarship, filter);
+      scholarship.show = name(scholarship, filters);
     } catch (err) {
-      console.error("Filter error caught: ", scholarship.name, err);
+      console.error("filters error caught: ", scholarship.name, err);
       scholarship.show = false;
     }
   });
+
+  return scholarships;
 }
 
-export function name(scholarship, filter) {
-  const visible = filter.name
-    ? scholarship.ScholarshipName.includes(filter.name)
+export function name(scholarship, filters) {
+  const visible = filters.name
+    ? scholarship.ScholarshipName.includes(filters.name)
     : true;
 
-  return visible ? false : date(scholarship, filter);
+  return visible ? false : date(scholarship, filters);
 }
 
-export function date(scholarship, filter) {
-  const visible = filter.date ? scholarship.ExpirationDate < filter.date : true;
+export function date(scholarship, filters) {
+  const visible = filters.date
+    ? scholarship.ExpirationDate < filters.date
+    : true;
 
-  return visible ? false : gPA(scholarship, filter);
+  return visible ? false : gPA(scholarship, filters);
 }
 
-export function gPA(scholarship, filter) {
+export function gPA(scholarship, filters) {
   let visible = true;
 
-  if (filter.minGPA) {
+  if (filters.minGPA) {
     visible =
-      scholarship.minGPA < filter.minGPA && filter.minGPA < scholarship.maxGPA;
+      scholarship.minGPA < filters.minGPA &&
+      filters.minGPA < scholarship.maxGPA;
   }
 
-  if (filter.maxGPA) {
+  if (filters.maxGPA) {
     visible =
-      scholarship.minGPA < filter.maxGPA && filter.maxGPA < scholarship.maxGPA;
+      scholarship.minGPA < filters.maxGPA &&
+      filters.maxGPA < scholarship.maxGPA;
   }
 
-  return visible ? false : backer(scholarship, filter);
+  return visible ? false : backer(scholarship, filters);
 }
 
-export function backer(scholarship, filter) {
-  const visible = filter.backer
-    ? scholarship.Provider.includes(filter.backer)
+export function backer(scholarship, filters) {
+  const visible = filters.backer
+    ? scholarship.Provider.includes(filters.backer)
     : true;
 
-  return visible ? false : date(scholarship, filter);
+  return visible ? false : date(scholarship, filters);
 }
 
-export function amount(scholarship, filter) {
-  const visible = filter.amount ? scholarship.Amount >= filter.amount : true;
+export function amount(scholarship, filters) {
+  const visible = filters.amount ? scholarship.Amount >= filters.amount : true;
 
-  return visible ? false : interests(scholarship, filter);
+  return visible ? false : interests(scholarship, filters);
 }
 
-export function Interests(scholarship, filter) {
-  const visible = filter.Interests
-    ? scholarship.Interests.includes(filter.interests)
+export function interests(scholarship, filters) {
+  const visible = filters.interests
+    ? scholarship.Interests.includes(filters.interests)
     : true;
 
-  return visible ? false : ethnicity(scholarship, filter);
+  return visible ? false : ethnicity(scholarship, filters);
 }
 
-export function ethnicity(scholarship, filter) {
-  const visible = filter.ethnicity
-    ? scholarship.Ethnicity.includes(filter.ethnicity)
+export function ethnicity(scholarship, filters) {
+  const visible = filters.ethnicity
+    ? scholarship.Ethnicity.includes(filters.ethnicity)
     : true;
 
-  return visible ? false : citizenship(scholarship, filter);
+  return visible ? false : citizenship(scholarship, filters);
 }
 
-export function citizenship(scholarship, filter) {
-  const visible = filter.citizenship
-    ? scholarship.CitizenshipStatus.includes(filter.citizenship)
+export function citizenship(scholarship, filters) {
+  const visible = filters.citizenship
+    ? scholarship.CitizenshipStatus.includes(filters.citizenship)
     : true;
 
-  return visible ? false : state(scholarship, filter);
+  return visible ? false : state(scholarship, filters);
 }
 
-export function state(scholarship, filter) {
-  const visible = filter.state
-    ? scholarship.StateOfResidency.includes(filter.state)
+export function state(scholarship, filters) {
+  const visible = filters.state
+    ? scholarship.StateOfResidency.includes(filters.state)
+    : true;
+
+  return visible ? false : education(scholarship, filters);
+}
+
+export function education(scholarship, filters) {
+  const visible = filters.education
+    ? scholarship.CollegeLevel.includes(filters.education)
     : true;
 
   return visible;
